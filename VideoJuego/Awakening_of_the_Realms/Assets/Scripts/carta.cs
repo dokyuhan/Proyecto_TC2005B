c46e99718d95f377beea4f7f66effb21e2664bc6
A01749746 <a01749746@tec.mx>
2024-04-03 14:01:34 -0600

    API para inventario

--- VideoJuego/Awakening_of_the_Realms/Assets/Scripts/carta.cs
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/carta.cs
@@ -1,38 +1,45 @@
 //script para creación de cartas
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
 
-[System.Serializable]
-public class Carta
-{
-
-    public int Card_Id;
-    public string Name;
-    public string Description;
-    public int Attack;
-    public int Defense;
-    public int Healing;
-    public int Power_Cost;
-    public int XP_Cost;
-    public string Realm;
-    public string Rarity;
-    public string Level;
-    public string Effect;
-    
-    // Añadido para manejar el estado de desbloqueo
-    //public bool IsUnlocked;
-    
-    public string nombre;
-    public string imagen; 
-    public bool desbloqueada;
-
-    public Carta(string name, string nombreImagen, bool bloqueada)
+   [System.Serializable]
+    public class Card
     {
-        nombre = name;
-        imagen = nombreImagen;
-        desbloqueada = bloqueada;
-    }
+        public int card_ID;
+        public string card_name;
+        public string card_description;
+        public int attack; // Ensure this matches with your data. You might need a custom parsing method if your data structure is complex.
+        public int defense; // Used for fetching the sprite
+        public int healing; // Used for fetching the sprite
+        public string card_realm; // Used for fetching the sprite
+        public int power_cost; // Used for fetching the sprite
+        public int exp_cost; // Used for fetching the sprite
+        public string rarity; // Used for fetching the sprite
+        public int card_level; // Used for fetching the sprite
+        public string Effect_type; // Used for fetching the sprite
+        public bool desbloqueada;
 
-}
+
+
+        /*public Card(int ID,string name, string desc, int att, int def, int heal, string rein, int power, int exp, string rarity, int lvl, string effect)
+        {
+            id = ID;
+            nombre = name;
+            descripcion = desc;
+            ataque = att;
+            defensa = def;
+            curacion = heal;
+            reino = rein;
+            poderCosto = power;
+            expCosto = exp;
+            rareza = rarity;
+            nivel = lvl;
+            efecto = effect;
+        }*/
+
+
+    }

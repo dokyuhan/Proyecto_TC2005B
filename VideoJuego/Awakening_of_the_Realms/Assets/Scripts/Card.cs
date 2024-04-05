0fb6835c3d33d3baa14e40d80cc638c2d7a5c2dd
A01749746 <a01749746@tec.mx>
2024-04-05 11:45:34 -0600

    Inventario con conexion a api

--- VideoJuego/Awakening_of_the_Realms/Assets/Scripts/Card.cs
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/Card.cs
@@ -6,38 +6,37 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 using TMPro;
 
-   [System.Serializable]
     public class Card
     {
         public int card_ID;
         public string card_name;
         public string card_description;
-        public int attack; // Ensure this matches with your data. You might need a custom parsing method if your data structure is complex.
-        public int defense; // Used for fetching the sprite
-        public int healing; // Used for fetching the sprite
-        public string card_realm; // Used for fetching the sprite
-        public int power_cost; // Used for fetching the sprite
-        public int exp_cost; // Used for fetching the sprite
-        public string rarity; // Used for fetching the sprite
-        public int card_level; // Used for fetching the sprite
-        public string Effect_type; // Used for fetching the sprite
+        public int attack;
+        public int defense;
+        public int healing;
+        public string card_realm;
+        public int power_cost;
+        public int exp_cost;
+        public string rarity;
+        public int card_level;
+        public string Effect_type;
         public bool desbloqueada;
 
-        public Card(int card_ID, string card_name, string card_description, int attack, int defense, int healing, string card_realm, int power_cost, int exp_cost, string rarity, int card_level, string Effect_type, bool desbloqueada)
+        public Card(int id, string nombre, string desc, int ataque, int defensa, int heal, string realm, int power, int exp, string rareza, int nivel, string efecto, bool desb)
     {
-        this.card_ID = card_ID;
-        this.card_name = card_name;
-        this.card_description = card_description;
-        this.attack = attack;
-        this.defense = defense;
-        this.healing = healing;
-        this.card_realm = card_realm;
-        this.power_cost = power_cost;
-        this.exp_cost = exp_cost;
-        this.rarity = rarity;
-        this.card_level = card_level;
-        this.Effect_type = Effect_type;
-        this.desbloqueada = desbloqueada;
+        card_ID = id;
+        card_name = nombre;
+        card_description = desc;
+        attack = ataque;
+        defense = defensa;
+        healing = heal;
+        card_realm = realm;
+        power_cost = power;
+        exp_cost = exp;
+        rarity = rareza;
+        card_level = nivel;
+        Effect_type = efecto;
+        desbloqueada = desb;
     }
 
 

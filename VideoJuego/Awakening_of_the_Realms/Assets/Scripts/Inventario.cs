f66bc2810b791880376b583c6c8c9314afdc0b22
A01749746 <a01749746@tec.mx>
2024-04-08 14:42:47 -0600

    cambios

--- VideoJuego/Awakening_of_the_Realms/Assets/Scripts/Inventario.cs
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/Inventario.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Inventario : MonoBehaviour
 {
@@ -15,9 +16,8 @@ public class Inventario : MonoBehaviour
 
     Card card;
 
-    public GameObject cardPrefab; 
-    public Transform inv;
 
+    public CardDisplayManager cardDisplayManager;
     public List<Card> cards = new List<Card>();
 
     // Assuming Card class is correctly structured to match the JSON response from Express.js
@@ -88,98 +88,22 @@ public class Inventario : MonoBehaviour
             yield return StartCoroutine(GetCard());
         }
 
-        // Una vez completado todo, muestra las cartas
-        DisplayCards();
-    }
-
-    void DisplayCards()
-    {
         foreach (Card card in cards)
         {
-            GameObject objeto = Instantiate(cardPrefab, inv);
-
-            Arrastrar arrastrarScript = objeto.GetComponent<Arrastrar>();
-
-            if (arrastrarScript != null)
-            {
-                arrastrarScript.cartaArrastrar = card; // Asigna la carta actual
-            }
-
-            //Image imagenCarta = objeto.transform.Find("background").GetComponent<Image>(); 
-
-            Image imagenCarta = objeto.transform.Find("background").GetComponent<Image>(); 
-            Sprite cardSprite = Resources.Load<Sprite>("Cards/" + card.card_name);
-            if (cardSprite != null)
-            {
-                imagenCarta.sprite = cardSprite;
-            }
-            else
-            {
-                Debug.LogWarning("No se encontro la imagen para: " + card.card_name);
-            }
-            
-            Image imagenMarco = objeto.transform.Find("marco").GetComponent<Image>(); 
-            Sprite frameSprite = Resources.Load<Sprite>("Frames/" + card.card_realm);
-            if (frameSprite != null)
-            {
-                imagenMarco.sprite = frameSprite;
-            }
-            else
-            {
-                Debug.LogWarning("No se encontro el marco para: " + card.card_realm);
-            }
-
-            TextMeshProUGUI energia = objeto.transform.Find("Energy").GetComponent<TextMeshProUGUI>();
-            if (energia != null)
-            {
-                energia.text = card.power_cost.ToString();
-            }
-
-            TextMeshProUGUI ataque = objeto.transform.Find("Attack").GetComponent<TextMeshProUGUI>();
-            if (ataque != null)
-            {
-                ataque.text = card.attack.ToString();
-            }
-
-            TextMeshProUGUI defensa = objeto.transform.Find("Defense").GetComponent<TextMeshProUGUI>();
-            if (defensa != null)
-            {
-                defensa.text = card.defense.ToString();
-            }
-
-            TextMeshProUGUI heal = objeto.transform.Find("Healing").GetComponent<TextMeshProUGUI>();
-            if (heal != null)
-            {
-                heal.text = card.healing.ToString();
-            }
-
-            TextMeshProUGUI descripcion = objeto.transform.Find("Descript").GetComponent<TextMeshProUGUI>();
-            if (descripcion != null)
-            {
-                descripcion.text = card.card_description.ToString();
-            }
-
-            TextMeshProUGUI nombre = objeto.transform.Find("Nombre").GetComponent<TextMeshProUGUI>();
-            if (nombre != null)
-            {
-                nombre.text = card.card_name.ToString();
-            }
-
-
-            
-
-            if (!card.desbloqueada)
-            {
-                imagenCarta.color = new Color(imagenCarta.color.r, imagenCarta.color.g, imagenCarta.color.b, 0.7f); 
-                imagenMarco.color = new Color(imagenMarco.color.r, imagenMarco.color.g, imagenMarco.color.b, 0.7f); 
-            }
+            cardDisplayManager.DisplayCards(card);
         }
+    }
+
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainScreen");
+    }
+
+    public void Save()
+    {
+        //aqui se debe hacer el post de la lista mazo;
     }
 }
 
 
-
-public class Controll : MonoBehaviour
-{
-
-}

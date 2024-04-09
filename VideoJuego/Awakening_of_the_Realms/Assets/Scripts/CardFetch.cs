a6ad136ffaadf4c1040390c2774c53f0aae5a677
Do Kyu Han <A01025119@tec.mx>
2024-04-09 01:39:29 -0600

    Cambios sobre el fetch de las cartas de Unity

---
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/CardFetch.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CardFetch : MonoBehaviour
+{
+    [SerializeField] private string apiURL = "localhost:3200";
+    [SerializeField] private string cardEndpoint = "/api/awakening/cards/";
+    private int cardId = 1;
+    Card card;
+
+    public List<Card> cards = new List<Card>(); 
+    /* delegate y event son clases que permiten utilizar los metodos como objetos en #C, en donde el delegate define una firma (OnCardsFetched) y recibe el tipo de parametro (en este caso seria la Lista) y regresa un void
+    Hay muchas maneras de uso del delegate y event, pero para este caso se esta utilizando para la visualizacion de las cartas especificas para cada mazo. Se utiliza la clase de delegate porque para otros casos (dentro del juego)
+    solo se necesitaria visualizar las cartas del deck y no todo el inventario. En resumen estas clases son utilizadas para mandar los datos especificos para cada mazo al igual de tener un mejor mantenimiento */
+    public delegate void OnCardsFetched(List<Card> cards);
+    public static event OnCardsFetched CardsFetched;
+
+    private void Start()
+    {
+        StartCoroutine(FetchCards());
+    }
+
+    IEnumerator FetchCards()
+    {
+        for (int i = 1; i <= 40; i++)
+        {
+            yield return StartCoroutine(GetCard(i));
+        }
+        
+        /* CardsFetched proviene del evento OnCardsFetched, que este evento en si contiene argumentos especificos (en este caso seria la List<Card> cards),
+        porque se pasan parametros especificos, cuando este evento es llamado en otra parte agrega dentro del evento el metodo que se desea correr o implementar (un ejemplo seria en el script del inventario en donde se programa un codigo 
+        utilizando la clase de CardFetch el evento CardsFetched y un operador que suma (+=) la funcion de  DisplayAllCards encontrada en inventario.) Con esos parametros recibidos se ejecuta los metodos del evento (en este caso se estaria ejecutando
+        la visualizacion de las cartas que la funcion mando, analiznado la funcion DisplayAllCards sabemos que se desea implementar una visualizacion de todas las cartas. En resumen el evento guardaria cardDisplayManager.DisplayCards(card) de cada carta que se mando en la lista de cartas.)
+        La clase Invoke sirve para encapsular exepciones, esto se refiere a que si cuando un evento no recibe ningun metodo o valor en lugar de mandar el error de un null, no se realize nada y no mande ningun output  */
+        CardsFetched?.Invoke(cards);
+    }
+
+    IEnumerator GetCard(int id)
+    {
+        UnityWebRequest www = UnityWebRequest.Get($"{apiURL}{cardEndpoint}{id}");
+
+        Debug.Log("URL: " + apiURL + cardEndpoint + cardId);
+
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("se conecto");
+            // If the request is successful, we parse the JSON data and store it in the card object
+            // The response of the request is stored in the downloadHandler property of the UnityWebRequest object
+            string data = www.downloadHandler.text;
+
+            // Using the JsonUtility class, we can parse the JSON data and store it in the card object
+            // It is important to note that the JSON data must match the structure of the Card class
+            card = JsonUtility.FromJson<Card>(data);
+
+            card.desbloqueada = true;
+
+            cards.Add(card);
+        }
+
+        else
+        {
+            Debug.Log($"Request failed: {www.error}");
+        }
+    }
+}
+
--- VideoJuego/Awakening_of_the_Realms/Assets/Scripts/Inventario.cs
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/Inventario.cs
@@ -1,93 +1,40 @@
-//script para creación de cartas en el inventario
-//script para creación de cartas
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Networking;
-using UnityEngine.UI;
-using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class Inventario : MonoBehaviour
-{
-    [SerializeField] string apiURL = "localhost:3200"; // Updated to match Express.js server
-    [SerializeField] string cardEndpoint = "/api/awakening/cards/"; // Endpoint adjusted
-    int cardId = 1;
+{   
+    /* Inventario es declarado como una Instancia que propiedades de un public getter y un private setter
+    Esta instancia es utilizada para implementar un singleton design pattern, que se refiere a que el inventario es unico para todo el juego */
+    public static Inventario Instance { get; private set; }
 
-    Card card;
+    public CardDisplayManager cardDisplayManager; 
 
-
-    public CardDisplayManager cardDisplayManager;
-    public List<Card> cards = new List<Card>();
-
-    // Assuming Card class is correctly structured to match the JSON response from Express.js
-    // Make sure this class matches the server's response structure, especially the naming
-
-    IEnumerator GetCard()
+    /* Funcion privada que utiliza Awake (inicializa cualquiera condicion antes de iniciar el juego)
+    Dentro de la funcion se esta inicializando condiciones para verificar si el inventario ya existe o no, si existe lo elimina, si no lo mantiene */
+    private void Awake()
     {
-        // We create a UnityWebRequest object and make a GET request to the API
-        // We use the cardEndpoint and cardId to fetch the card data
-        UnityWebRequest www = UnityWebRequest.Get(apiURL + cardEndpoint + cardId);
-
-        Debug.Log("URL: " + apiURL + cardEndpoint + cardId);
-
-        yield return www.SendWebRequest();
-
-        // If the request fails, we log the error
-        if(www.result != UnityWebRequest.Result.Success)
+        if (Instance == null)
         {
-            Debug.Log($"Request failed: {www.error}");
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            CardFetch.CardsFetched += DisplayAllCards;
         }
-        else 
+        else
         {
-            Debug.Log("se conecto");
-            // If the request is successful, we parse the JSON data and store it in the card object
-            // The response of the request is stored in the downloadHandler property of the UnityWebRequest object
-            string data = www.downloadHandler.text;
-
-            // Using the JsonUtility class, we can parse the JSON data and store it in the card object
-            // It is important to note that the JSON data must match the structure of the Card class
-            card = JsonUtility.FromJson<Card>(data);
-
-            card.desbloqueada = true;
-
-            cards.Add(card);
-
-            cardId++;
-
-
-
-
-            // Using the card object we can display the card data in the UI
-            /*id.text = card.card_ID.ToString();
-            nombre.text = card.card_name;
-            descripcion.text = card.card_description;
-            ataque.text = card.attack.ToString();
-            defensa.text = card.defense.ToString();
-            curacion.text = card.healing.ToString();
-            reino.text = card.card_realm;
-            poderCosto.text = card.power_cost.ToString();
-            expCosto.text = card.exp_cost.ToString();
-            rareza.text = card.rarity;
-            nivel.text = card.card_level.ToString();
-            efecto.text = card.Effect_type;*/
-
-
-            //cardImage.sprite = Resources.Load<Sprite>($"Spells/{card.card_id}");
-            
-            //Debug.Log($"Name: {card.card_name} Description: {card.card_description} Cost: {card.card_cost}");
+            Destroy(gameObject);
         }
     }
-
-
-    IEnumerator Start()
+    
+    // Funcion que corre cuando el obejto es destruido
+    private void OnDestroy()
     {
-        for (int i = 1; i <= 40; i++)
-        {
-            // Espera a que GetCard termine antes de continuar
-            yield return StartCoroutine(GetCard());
-        }
+        CardFetch.CardsFetched -= DisplayAllCards;
+    }
 
+    public void DisplayAllCards(List<Card> cards)
+    {
         foreach (Card card in cards)
         {
             cardDisplayManager.DisplayCards(card);

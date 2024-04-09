130650cb03730bf3d7b83b131fffef3bc39d6e7e
A01749746 <a01749746@tec.mx>
2024-04-09 00:35:43 -0600

    Added endpoints para inventario y modificacion scripts

--- VideoJuego/Awakening_of_the_Realms/Assets/Scripts/APIConnection.cs
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/APIConnection.cs
@@ -1,10 +1,19 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 
 public class APIConnection : MonoBehaviour
 {
+    //Variables para AddUser
     [SerializeField] private string apiURL = "http://localhost:3200";
+
+    //variables para GetCard()
+    int cardId = 1;
+    Card card;
+    public List<Card> cards = new List<Card>();
+
 
     public IEnumerator AddUser(string endpoint, string jsonData, System.Action<bool, string> callback)
     {
@@ -25,5 +34,61 @@ public class APIConnection : MonoBehaviour
             }
         }
     }
+
+    public IEnumerator GetCards()
+    {
+        for (int i = 1; i <= 40; i++)
+        {
+            UnityWebRequest www = UnityWebRequest.Get(apiURL + "/api/awakening/cards/" + cardId);
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError($"Failed to fetch card {cardId}: {www.error}");
+            }
+            else
+            {
+                string data = www.downloadHandler.text;
+                card = JsonUtility.FromJson<Card>(data);
+                card.desbloqueada = true;
+                cards.Add(card);
+            }
+
+            cardId++;
+        }
+    }
+
+
+    public IEnumerator AddCardsToDeck(string endpoint, List<int> cardIDs, int playerID, int deckID, Action<bool, string> callback)
+    {
+        // Crear la lista de objetos card para el JSON basado en cardIDs
+        var cardsData = new List<object>();
+        foreach (var cardID in cardIDs)
+        {
+            cardsData.Add(new { card_ID = cardID, player_ID = playerID, deck_ID = deckID });
+        }
+        
+        // Crear el objeto a serializar
+        var payload = new { cards = cardsData };
+        string jsonData = JsonUtility.ToJson(payload, true);
+
+        using (UnityWebRequest www = UnityWebRequest.Put(apiURL + endpoint, jsonData))
+        {
+            www.method = "POST";
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                callback(true, www.downloadHandler.text);
+            }
+            else
+            {
+                callback(false, www.error);
+            }
+        }
+    }
+
 }
 
--- VideoJuego/Awakening_of_the_Realms/Assets/Scripts/Inventario.cs
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/Inventario.cs
@@ -10,89 +10,36 @@ using UnityEngine.SceneManagement;
 
 public class Inventario : MonoBehaviour
 {
-    [SerializeField] string apiURL = "localhost:3200"; // Updated to match Express.js server
-    [SerializeField] string cardEndpoint = "/api/awakening/cards/"; // Endpoint adjusted
-    int cardId = 1;
-
-    Card card;
-
 
     public CardDisplayManager cardDisplayManager;
-    public List<Card> cards = new List<Card>();
-
-    // Assuming Card class is correctly structured to match the JSON response from Express.js
-    // Make sure this class matches the server's response structure, especially the naming
-
-    IEnumerator GetCard()
-    {
-        // We create a UnityWebRequest object and make a GET request to the API
-        // We use the cardEndpoint and cardId to fetch the card data
-        UnityWebRequest www = UnityWebRequest.Get(apiURL + cardEndpoint + cardId);
-
-        Debug.Log("URL: " + apiURL + cardEndpoint + cardId);
-
-        yield return www.SendWebRequest();
-
-        // If the request fails, we log the error
-        if(www.result != UnityWebRequest.Result.Success)
-        {
-            Debug.Log($"Request failed: {www.error}");
-        }
-        else 
-        {
-            Debug.Log("se conecto");
-            // If the request is successful, we parse the JSON data and store it in the card object
-            // The response of the request is stored in the downloadHandler property of the UnityWebRequest object
-            string data = www.downloadHandler.text;
-
-            // Using the JsonUtility class, we can parse the JSON data and store it in the card object
-            // It is important to note that the JSON data must match the structure of the Card class
-            card = JsonUtility.FromJson<Card>(data);
-
-            card.desbloqueada = true;
-
-            cards.Add(card);
-
-            cardId++;
-
-
-
-
-            // Using the card object we can display the card data in the UI
-            /*id.text = card.card_ID.ToString();
-            nombre.text = card.card_name;
-            descripcion.text = card.card_description;
-            ataque.text = card.attack.ToString();
-            defensa.text = card.defense.ToString();
-            curacion.text = card.healing.ToString();
-            reino.text = card.card_realm;
-            poderCosto.text = card.power_cost.ToString();
-            expCosto.text = card.exp_cost.ToString();
-            rareza.text = card.rarity;
-            nivel.text = card.card_level.ToString();
-            efecto.text = card.Effect_type;*/
-
-
-            //cardImage.sprite = Resources.Load<Sprite>($"Spells/{card.card_id}");
-            
-            //Debug.Log($"Name: {card.card_name} Description: {card.card_description} Cost: {card.card_cost}");
-        }
-    }
+    public GameObject apiConnectionGameObject;
+    private APIConnection apiConnection;
+    public List<Card> mazo = new List<Card>();
 
 
     IEnumerator Start()
     {
-        for (int i = 1; i <= 40; i++)
-        {
-            // Espera a que GetCard termine antes de continuar
-            yield return StartCoroutine(GetCard());
-        }
+       // Obtén una referencia al script APIConnection adjunto al GameObject
+        apiConnection = apiConnectionGameObject.GetComponent<APIConnection>();
 
-        foreach (Card card in cards)
+        // Llama al método GetCards y espera a que termine
+        yield return StartCoroutine(apiConnection.GetCards());
+
+        // Muestra todas las cartas obtenidas
+        foreach (Card card in apiConnection.cards)
         {
             cardDisplayManager.DisplayCards(card);
         }
+
+        foreach (Card card in apiConnection.cards)
+        {
+            mazo.Add(card);
+        }
+
+
+
     }
+
 
 
     public void Back()
@@ -102,8 +49,12 @@ public class Inventario : MonoBehaviour
 
     public void Save()
     {
-        //aqui se debe hacer el post de la lista mazo;
+    
     }
+
+
+
+
 }
 
 

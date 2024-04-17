9792d26678a60908f670035379c7365ada7d5a80
Do Kyu Han <A01025119@tec.mx>
2024-04-17 12:06:25 -0600

    Modificaciones a las escenas del Juego

--- VideoJuego/Awakening_of_the_Realms/Assets/Scripts/SceneController.cs
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/SceneController.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -58,4 +58,4 @@ public class SceneController : MonoBehaviour
 
         Debug.Log("Level " + level.name + " loaded with AI setting " + level.aiLevel);
     }
-}*/
+}

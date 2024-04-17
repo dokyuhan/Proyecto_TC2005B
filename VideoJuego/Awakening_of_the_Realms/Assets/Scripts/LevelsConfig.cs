27917ba5111ff05159ef0f931afcb3b325dcb484
Do Kyu Han <A01025119@tec.mx>
2024-04-17 12:03:00 -0600

    Modificaciones a las escenas del Juego
    :Q

---
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/LevelsConfig.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "LevelsConfig", menuName = "Configuration/LevelsConfig")]
+public class LevelsConfig : ScriptableObject {
+    public Level[] levels;
+}
+
+[System.Serializable]
+public class Level {
+    public string name;
+    public AILevel aiLevel;
+    public Material background; // Reference to the background Material
+}
--- VideoJuego/Awakening_of_the_Realms/Assets/Scripts/SelectLevel.cs
+++ VideoJuego/Awakening_of_the_Realms/Assets/Scripts/SelectLevel.cs
@@ -2,17 +2,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SelectLevel : MonoBehaviour {
-
-    public string[] levelScenes; // Array to store the names of the scenes for each level
-
+    public int levelScenes;
     public void SelectGameLevel(int levelIndex) {
+        PlayerPrefs.SetInt("SelectedLevel", levelIndex);
+        PlayerPrefs.Save();
         // Check if the level index is within the range of defined scenes
-        if (levelIndex >= 0 && levelIndex < levelScenes.Length) {
-            // Load the scene corresponding to the selected level
-            SceneManager.LoadScene(levelScenes[levelIndex]);
-        } else {
-            Debug.LogError("Invalid level index or scene not configured");
-        }
+        SceneManager.LoadScene("GameLevel");
     }
 
     public void Back() {
